1995ea837b6deacab3713fd2c8bce55c4d6170c4
Farzin Adil <54300496+farzinadil@users.noreply.github.com>
2021-04-19 19:11:41 -0700

    added ActionListner to layerController.java

--- src/BoardView.java
+++ src/BoardView.java
@@ -1,4 +1,24 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionListener;
 
-public class BoardView {
+public class BoardView extends JFrame implements ChangeListener {
+    MancalaModel mancalaModel;
+    public BoardView (MancalaModel mancalaModel){
+        this.mancalaModel = mancalaModel;
+        //add(addMouseListener();)
+    }
+    /*
+    void addCalculateListener(ActionListener listenForCalcButton){
+        calculateButton.addActionListener(listenForCalcButton);
+    }
+    */
 
+
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+
+    }
 }
--- src/PlayerController.java
+++ src/PlayerController.java
@@ -1,12 +1,24 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class PlayerController {
     private MancalaModel mancalaModel;
     private BoardView boardView;
     public PlayerController(MancalaModel mancalaModel, BoardView boardView){
         this.mancalaModel = mancalaModel;
         this.boardView = boardView;
+
+        //this.boardView.addInputListner(new InputListner());
     }
     public void updateBoardView(){
 
+    }
+    class  InputListner implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 
 }
