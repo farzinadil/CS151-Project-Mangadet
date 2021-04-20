476a899a14897fd7a1293eacd9b94e2a99cf5f41
Farzin Adil <54300496+farzinadil@users.noreply.github.com>
2021-04-19 17:52:00 -0700

    defined constructor and update method in PlayerController.java

--- src/PlayerController.java
+++ src/PlayerController.java
@@ -1,2 +1,12 @@
 public class PlayerController {
+    private MancalaModel mancalaModel;
+    private BoardView boardView;
+    public PlayerController(MancalaModel mancalaModel, BoardView boardView){
+        this.mancalaModel = mancalaModel;
+        this.boardView = boardView;
+    }
+    public void updateBoardView(){
+
+    }
+
 }
