1e9b7920d0b7b716c8a9070680e6503c4371b8e0
Farzin Adil <54300496+farzinadil@users.noreply.github.com>
2021-05-05 00:04:34 -0700

    reassigned branches

--- src/MacalaTest.java
+++ src/MacalaTest.java
@@ -1,6 +1,6 @@
 public class MacalaTest {
     public static void main (String args[]){
-        Style style = new StyleTest();
+        Style style = new Style1();
         MancalaModel mancalaModel = new MancalaModel();
         PlayerController controller = new PlayerController(mancalaModel);
         BoardView boardView = new BoardView(mancalaModel, controller, style);
