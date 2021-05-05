9d398ffc7dd7aadb3bf4c03d5cf9a70b0f29df32
shayna-gaulden <shayna.gaulden@gmail.com>
2021-05-05 16:27:28 -0700

    hi

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -1,8 +1,19 @@
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class MancalaModel {
 	// Position A1-A6 is 0-5, B1-B6 is 7-12,
 	// 6 is the main pit for A, and 13 is the main pit for B
 	private int[] pits = new int[14];
+	private ChangeListener listener;
+	
+	
+	MancalaModel() {
+		// set all pits to 0
+		for (int i = 0; i < 14; i++) {
+			pits[i]=0;
+		}
+	}
 	
 	/**
 	 * moves the stones recursively for one turn.
@@ -11,26 +22,22 @@ public class MancalaModel {
 	 * @return true if the player gets an extra turn, false if not
 	 */
 	public boolean move(int position, int player) {
-		int hand = pits[position]; // this is like picking up the stones
-		pits[position] = 0;
+		int hand = getStones(position); // this is like picking up the stones
+		setPits(position,0);
 		int pos = position;
 		boolean extraTurn = false; // will return true if player gets an extra turn
-		// purposefully does not put all stones so the final stone can be done
-		// outside of the for loop
-		//for (int i = position+1; i<position+hand; i++) {
-		//	if (position==6 && player == 0)
-		//}
+
 		while(hand>0) {
 			// First check if we are in mancala pit
 			if (pos == 6 || pos == 13) {
 				// only put here if its player A's turn
 				if (pos == 6 && player == 0) {
-					pits[pos]++;
+					setPits(pos,getStones(pos)+1);
 					hand--;
 				}
 				// only put here if its player B's turn
 				else if (pos == 13 && player == 1) {
-					pits[pos]++;
+					setPits(pos,getStones(pos)+1);
 					hand--;
 				}
 				// hand will only be 0 here if the last stone has just been put into a mancala pit
@@ -39,27 +46,27 @@ public class MancalaModel {
 				}
 			}
 			else {
-				pits[pos]++;
+				setPits(pos,getStones(pos)+1);
 				hand--;
 				// need to check if the last stone has been placed.
 				// need to check if a steal is possible
 				if (hand == 0) {
 					// player A
 					if (player == 0 && (pos >= 0 && pos <= 5)) {
-						if (pits[pos] == 1 && pits[pos+7]>0) {
+						if (getStones(pos) == 1 && getStones(pos+7)>0) {
 							// put into player A's mancala
-							pits[0] += pits[pos+7]+1;
-							pits[pos] = 0;
-							pits[pos+7] = 0;
+							setPits(0,getStones(0) + getStones(pos+7)+1);
+							setPits(pos,0);
+							setPits(pos+7,0);
 						}
 					}
 					// player B
 					else if (player == 1 && (pos >= 7 && pos <= 12)) {
-						if (pits[pos] == 1 && pits[pos+7]>0) {
+						if (getStones(pos) == 1 && getStones(pos+7)>0) {
 							// put into player B's mancala
-							pits[6] += pits[pos-7]+1;
-							pits[pos] = 0;
-							pits[pos-7] = 0;
+							setPits(6,getStones(pos) + getStones(pos-7)+1);
+							setPits(pos,0);
+							setPits(pos-7, 0);
 						}
 					}
 				}
@@ -67,7 +74,25 @@ public class MancalaModel {
 		}
 		return extraTurn;
 	}
+	
+	public void setPits(int position, int value) {
+		// Update data
+		pits[position] = value;
+		// Notify view
+		ChangeEvent event = new ChangeEvent(this);
+		listener.stateChanged(event);
+	}
+	
 	public int[] getPits(){
 		return pits;
+	}
+	
+	public int getStones(int i) {
+		return pits[i];
+	}
+
+	public void setListener(ChangeListener listener) {
+		this.listener=listener;
+		
 	}
 }
