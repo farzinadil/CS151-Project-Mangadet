af7c8919c50d6da033731db3568bd4c7b3326bc3
Kevin Koo <kevinkoo823@gmail.com>
2021-05-06 10:00:34 -0700

    delete redundant test class

--- src/MancalaTest.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame;
-
-public class MancalaTest {
-	public static void main (String args[]){
-	    	SetupView c = new SetupView();
-	    	c.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    	c.setVisible(true);
-	    }
-}
